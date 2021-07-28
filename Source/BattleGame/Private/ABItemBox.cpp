88be694497dd290b0a517c80ac16cd8b330cd922
RightDay <spreadday@gmail.com>
2021-07-28 22:56:08 +0900

    WeaponItemClass 변경

--- Source/BattleGame/Private/ABItemBox.cpp
+++ Source/BattleGame/Private/ABItemBox.cpp
@@ -38,7 +38,11 @@ AABItemBox::AABItemBox()
 	Trigger->SetCollisionProfileName(TEXT("ItemBox"));
 	Box->SetCollisionProfileName(TEXT("NoCollision"));
 
-	WeaponItemClass = AABWeapon::StaticClass();
+	static ConstructorHelpers::FClassFinder<AABWeapon> U_WEAPON(TEXT("Blueprint'/Game/Book/Blueprints/BP_WeaponAxe.BP_WeaponAxe_C'"));
+	if (U_WEAPON.Succeeded())
+	{
+		WeaponItemClass = U_WEAPON.Class;
+	}
 }
 
 // Called when the game starts or when spawned
